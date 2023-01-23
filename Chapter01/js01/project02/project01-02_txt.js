b6c867be90e64a0f0bf31ee726672c8e88fc1d36
Malachi Maenle <malachi.maenle@sscinc.com>
2023-01-23 13:22:50 -0600

    first commit

---
+++ Chapter01/js01/project02/project01-02_txt.js
@@ -0,0 +1,11 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Malachi Maenle
+    Date:   1/23/2023
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
