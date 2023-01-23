b332deab62c470ac92905090361f788c1320953c
Malachi Maenle <malachi.maenle@sscinc.com>
2023-01-23 14:08:40 -0600

    Hands-on Project 1-2

--- Chapter01/project02/project01-02_txt.js
+++ Chapter01/project02/project01-02_txt.js
@@ -9,3 +9,12 @@
 */
 
 //define variables for service name and service speed
+
+var service1Name = "Basic"
+var service2Name = "Express"
+var service3Name = "Extreme"
+var service4Name = "Ultimate"
+var service1Speed = "50 Mbps"
+var service2Speed = "100Mbps"
+var service3Speed = "500 Mbps"
+var service4Speed = "1 Gig"
